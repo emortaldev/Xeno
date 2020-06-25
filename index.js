c72c0746bc670b6f5a890b4b11c6c53c52600869
Emortal <nathanpenwill88@gmail.com>
2020-06-25 11:41:10 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
-// If you're looking for source code, here is not the place.
-// Xeno is not open source and will likely never be.
-// Sorry about that!
+// Error 404
+// No source code for you
+// :)
