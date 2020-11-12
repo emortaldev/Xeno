f9654473ec579a9a50b096b64a936a6853817fe8
emortal <nathanpenwill88@gmail.com>
2020-11-12 20:07:50 +0000

    beautify

--- msparse.js
+++ msparse.js
@@ -1,1 +1,6 @@
-module.exports = (o => { o = parseInt(o / 1e3); const a = Math.floor(o / 3600), t = Math.floor(o % 3600 / 60); let N = `${0 != a ? a + "h " : ""}${0 != t ? t + "m " : ""}${Math.floor(o % 60)}s`; return "0s" == N && (N = "LIVE"), N; });
+module.exports = (o => {
+  o = parseInt(o / 1000);
+  const a = Math.floor(o / 3600),
+    t = Math.floor(o % 3600 / 60);
+  return `${0 != a ? a + "h " : ""}${0 != t ? t + "m " : ""}${Math.floor(o % 60)}s`;
+});
