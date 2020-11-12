14d58438073cb87aeae42db7954e6697484c6609
emortal <nathanpenwill88@gmail.com>
2020-11-12 20:45:04 +0000

    Update stats.js

--- commands/Info/stats.js
+++ commands/Info/stats.js
@@ -1,14 +1,14 @@
 const ms = require("../../msparse");
 
 module.exports.run = async (client, message) => {
-    const heapUsed = Math.floor(process.memoryUsage().heapUsed / 1024 / 1024);
-    const rssUsed = Math.floor(process.memoryUsage().rss / 1024 / 1024);
+    const heapUsed = Math.floor(process.memoryUsage().heapUsed / 1048576);
+    const rssUsed = Math.floor(process.memoryUsage().rss / 1048576);
 
     message.channel.send({
         embed: {
             fields: [
-                { name: "Ping", value: `🖥️ Ping \`${client.ws.ping}ms\`\n⏱️ Uptime \`${ms(Math.floor(client.uptime / 1e3) * 1e3)}\``, inline: true },
-                { name: "Usage", value: `RAM Used \`${heapUsed} MB\`\nRAM Allocated \`${rssUsed} MB\``, inline: true }
+                { name: "Ping", value: `🖥️ Ping \`${client.ws.ping}ms\`\n⏱️ Uptime \`${ms(Math.floor(client.uptime / 1000) * 1000)}\``, inline: true },
+                { name: "RAM Usage", value: `Used \`${heapUsed} MB\`\nAllocated \`${rssUsed} MB\``, inline: true }
             ],
             color: 0x7289DA,
             footer: { text: `${client.guilds.cache.size} servers` }
